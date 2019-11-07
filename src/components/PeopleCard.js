66a15d3d528876b2b8eba9c86b4df3261a63423e
Dzheyhan <dzheyhan.ahmedov@factor-a.de>
2019-11-07 10:52:51 +0100

    added style

--- src/components/PeopleCard.js
+++ src/components/PeopleCard.js
@@ -1,13 +1,16 @@
 import { RowCard, RowCardImg } from './Card';
 import { Card } from 'react-bootstrap';
 import React from 'react';
+import { StyledTitle } from './styled';
 
 function PeopleCard(props) {
   return (
     <RowCard>
       <RowCardImg variant="left" src={props.img} maxwidth="100px" />
       <Card.Body>
-        <h6>{props.name}</h6>
+        <StyledTitle>
+          <h6>{props.name}</h6>
+        </StyledTitle>
       </Card.Body>
     </RowCard>
   );
